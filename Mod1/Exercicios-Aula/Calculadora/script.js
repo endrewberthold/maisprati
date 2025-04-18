6d5991b1a118c7b83c91ca7470c59890b9de3622
endrew <endrew.berthold@gmail.com>
2025-04-18 18:21:44 -0300

    Acrescentando comentarios no código

--- Mod1/Exercicios-Aula/Calculadora/script.js
+++ Mod1/Exercicios-Aula/Calculadora/script.js
@@ -6,18 +6,33 @@ function insert(num){
     const operadores = ['+', '-', '*', '/', '.', ','];
     const limite = 12;
 
-    if (resultadoGerado && !operadores.includes(num)){
-        return;
-    }
-    if (resultadoGerado && operadores.includes(num)){
-        resultadoGerado = false;
-    }
+    //Define o limite de caracteres no display
     if (atual.length >= limite){
         return;
     }
+    /*
+    Regra para bloquear a entrada de operadores
+    como primeiro caractere, exceto "-"
+     */
     if (atual.length === 0 && operadores.includes(num) && num !== "-"){
         return;
     }
+    /*
+    Regra para validar se um resultado foi gerado e o usuário digitou um
+    operador, permitindo continuar a operação
+     */
+    if (resultadoGerado && operadores.includes(num)){
+        resultadoGerado = false;
+    }
+    /*
+    Regra para bloquear a inserção de numeros depois de gerar um resultado
+    */
+    if (resultadoGerado && !operadores.includes(num)){
+        return;
+    }
+    /*
+    Regra para impedir operadores em sequencia
+     */
     let ultimo = atual[atual.length - 1];
     if(operadores.includes(ultimo) && operadores.includes(num)){
         return;
