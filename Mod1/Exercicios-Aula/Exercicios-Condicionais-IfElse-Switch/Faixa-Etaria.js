aea48b65509071e7059dfdb0b6731d01f1a6df68
endrew <endrew.berthold@gmail.com>
2025-04-23 19:43:43 -0300

    Atualizando exercicios com condicionais

--- Mod1/Exercicios-Aula/Exercicios-Condicionais-IfElse-Switch/Faixa-Etaria.js
+++ Mod1/Exercicios-Aula/Exercicios-Condicionais-IfElse-Switch/Faixa-Etaria.js
@@ -1,38 +1,51 @@
+//caso com if else
 const prompt = require('prompt-sync')()
 
 let nome = prompt('Digite o nome: ')
 let idade = Number(prompt('Digite o idade: '))
 
-if(typeof nome === 'string' && idade > 0 && idade < 111) {
+if(typeof nome === 'string' && idade >= 0 && idade < 111) {
     if(idade >= 0 && idade <= 12) {
-        let faixa = 1;
+        console.log(`${nome} tem ${idade} anos e está na faixa: infantil.`);
     } else if (idade >= 13 && idade <= 17) {
-        let faixa = 2;
+        console.log(`${nome} tem ${idade} anos e está na faixa: adolescente.`);
     } else if (idade >= 18 && idade <= 21) {
-        let faixa = 3;
+        console.log(`${nome} tem ${idade} anos e está na faixa: jovem.`);
     } else if (idade >= 22 && idade <= 64) {
-        let faixa = 4;
+        console.log(`${nome} tem ${idade} anos e está na faixa: adulto.`);
     } else if (idade >= 65 && idade <= 110) {
-        let faixa = 5;
+        console.log(`${nome} tem ${idade} anos e está na faixa: idoso.`);
     }
 } else {
     console.log("Valores inválidos");
 }
-switch (faixa) {
-    case 1:
-        console.log(`${nome} tem ${idade} anos e está na faixa: infantil.`);
+
+//caso com switch case
+switch (idade) {
+    case idade:
+        if(idade >= 0 && idade <= 12) {
+            console.log(`${nome} tem ${idade} anos e está na faixa: infantil.`);
+        }
         break
-    case 2:
-        console.log(`${nome} tem ${idade} anos e está na faixa: adolescente.`);
+    case idade:
+        if(idade >= 13 && idade <= 17) {
+            console.log(`${nome} tem ${idade} anos e está na faixa: adolescente.`);
+        }
         break
-    case 3:
-        console.log(`${nome} tem ${idade} anos e está na faixa: jovem.`);
+    case idade:
+        if(idade >= 18 && idade <= 21) {
+            console.log(`${nome} tem ${idade} anos e está na faixa: jovem.`);
+        }
         break
-    case 4:
-        console.log(`${nome} tem ${idade} anos e está na faixa: adulto.`);
+    case idade:
+        if(idade >= 22 && idade <= 64) {
+            console.log(`${nome} tem ${idade} anos e está na faixa: adulto.`);
+        }
         break
-    case 5:
-        console.log(`${nome} tem ${idade} anos e está na faixa: idoso.`);
+    case idade:
+        if(idade >= 65 && idade <= 110) {
+            console.log(`${nome} tem ${idade} anos e está na faixa: idoso.`);
+        }
         break
     default:
         console.log("Valores invalidos");
